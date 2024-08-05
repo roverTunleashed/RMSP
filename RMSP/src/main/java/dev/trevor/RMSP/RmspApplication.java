bb07857a4993ab3e8101c3d376a433b5fdd3d66d
Trevor Pinto <74032637+roverTunleashed@users.noreply.github.com>
2024-08-04 21:09:40 -0700

    first commit

---
+++ RMSP/src/main/java/dev/trevor/RMSP/RmspApplication.java
@@ -0,0 +1,13 @@
+package dev.trevor.RMSP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmspApplication.class, args);
+	}
+
+}
---
+++ RMSP/src/test/java/dev/trevor/RMSP/RmspApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.trevor.RMSP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
