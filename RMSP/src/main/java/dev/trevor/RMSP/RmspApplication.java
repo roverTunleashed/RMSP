572bfb0e4b02210d8d681828a4d6ef9d8e23e893
Trevor Pinto <74032637+roverTunleashed@users.noreply.github.com>
2024-09-22 10:11:25 -0700

    test commit to backend branch

--- RMSP/src/main/java/dev/trevor/RMSP/RmspApplication.java
+++ RMSP/src/main/java/dev/trevor/RMSP/RmspApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RmspApplication {
-	//COmmit test
+	//Commit test
 	public static void main(String[] args) {
 		SpringApplication.run(RmspApplication.class, args);
 	}
