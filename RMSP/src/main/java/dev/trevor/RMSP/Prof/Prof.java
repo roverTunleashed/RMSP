17b416baf07c89cbcb952c43d28439d6f1890357
Trevor Pinto <74032637+roverTunleashed@users.noreply.github.com>
2025-02-28 01:24:33 -0800

    removed unused imports

--- RMSP/src/main/java/dev/trevor/RMSP/Prof/Prof.java
+++ RMSP/src/main/java/dev/trevor/RMSP/Prof/Prof.java
@@ -1,8 +1,6 @@
 package dev.trevor.RMSP.Prof;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
