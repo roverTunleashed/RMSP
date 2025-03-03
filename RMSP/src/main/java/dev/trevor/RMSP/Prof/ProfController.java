93fdc12dbbeba89659820ac872c315c6353264e1
Trevor Pinto <74032637+roverTunleashed@users.noreply.github.com>
2025-03-03 00:02:07 -0800

    fixed cors issue

--- RMSP/src/main/java/dev/trevor/RMSP/Prof/ProfController.java
+++ RMSP/src/main/java/dev/trevor/RMSP/Prof/ProfController.java
@@ -3,6 +3,7 @@ package dev.trevor.RMSP.Prof;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("v1/api/profs")
 public class ProfController {
     private final ProfService profService;
